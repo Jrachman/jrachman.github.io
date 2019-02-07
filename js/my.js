b066bbf058b98b6dbda54c24e355d7f25f99b3ac
Julian Rachman <jmrachman@gmail.com>
2019-02-07 15:12:20 -0800

    added image to email link

---
+++ js/my.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('a').mouseover(function(){
+        var color = $(this).text().trim();
+        //alert(color);
+        //$('.box').css('background',color);
+        $(this).animate({left:"-42px"},{duration:200}).delay(190)
+               .animate({zIndex:"2"},{duration:10})
+               .animate({left:"0px"},{duration:200});
+        });
+
+    $('a').mouseout(function(){
+
+        $(this).animate({left:"-42px"},{queue:false,duration:200}).delay(190)
+               .animate({zIndex:"0"},{duration:10})
+               .animate({left:"0px"},{duration:200});
+    });
+});
